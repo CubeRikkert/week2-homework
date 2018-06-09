335228d84ae5a3edf09513f1c1fc083cc7e573f6
CubeRikkert <ricksamuel@gmail.com>
2018-06-09 20:56:25 +0200

    pathFind.js function structure set, not working as it should yet, next step is: get it working :)

--- src/pathFind.js
+++ src/pathFind.js
@@ -1,12 +1,35 @@
-const pathFind = pathFind(path, object) {
-  
+function pathFind(path, object) {
+  let newObj = []
+  let currentPathKey = 0
+  let lookForIt = Object.keys(object).reduce((newObj, objKey) => {
+    if (objKey.value === path[0]) {
+      return newObj = [ objKey.value ]
+      currentPathKey++
+      console.log('first statement')
+    } if (objKey.value === path[currentPathKey]) {
+      return newObj = [ objKey.value ]
+      console.log('second statement')
+    } return NewObj
+    console.log('last statement')
+  })
+  return newObj
 }
 
 
 
+console.log(pathFind(['is', 'this', 'working?'], {
+  is: {
+    this: {
+      working: "Yes!"
+    }
+  }
+}))
 
 
 
 
 
-module.export.pathFind = pathFind
+
+
+
+//module.export.pathFind = pathFind
